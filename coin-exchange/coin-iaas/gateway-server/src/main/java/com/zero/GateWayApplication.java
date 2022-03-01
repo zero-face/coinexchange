06a43745af2c0fe7390112a6649f4e2bc2c5f024
zero <1444171773@qq.com>
2022-03-01 21:52:41 +0800

    feat(authorization-server): 随便优化一下

--- coin-exchange/coin-iaas/gateway-server/src/main/java/com/zero/GateWayApplication.java
+++ coin-exchange/coin-iaas/gateway-server/src/main/java/com/zero/GateWayApplication.java
@@ -1,5 +1,6 @@
 package com.zero;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @description
  * @since 1.8
  **/
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
