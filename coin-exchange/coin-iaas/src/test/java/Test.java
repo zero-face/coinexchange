dd8b2a8c39b4ca942639004a73561c378e7dc49f
zero <1444171773@qq.com>
2022-03-03 12:20:13 +0800

    feat(authorization-server): log4j bug复现

---
+++ coin-exchange/coin-iaas/src/test/java/Test.java
@@ -0,0 +1,15 @@
+
+
+/**
+ * @author Zero
+ * @date 2022/3/2 20:00
+ * @description
+ * @since 1.8
+ **/
+//public class Test {
+//    private static final Logger LOGGER = LogManager.getLogger();
+//    public static void main(String[] args) {
+//        String username = "${java:os}";
+//        LOGGER.error("test,{}", username);
+//    }
+//}
